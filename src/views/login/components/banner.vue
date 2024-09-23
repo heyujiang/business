ec385a0598e66e9ba5c17499f513beed86e855be
jiangyu <heyujiang@autowise.ai>
2024-09-23 11:33:49 +0800

    feat: 登录页banner

--- src/views/login/components/banner.vue
+++ src/views/login/components/banner.vue
@@ -2,11 +2,25 @@
   <div class="banner">
     <div class="banner-inner">
       <a-carousel class="carousel" animation-name="fade" :auto-play="{interval:3000,hoverToPause:true}">
-        <a-carousel-item v-for="item in carouselItem" :key="item.title">
-          <div :key="item.title" class="carousel-item">
-            <div class="carousel-title">{{ item.title }}</div>
-            <div class="carousel-sub-title">{{ item.des }}</div>
-            <img class="carousel-image" :src="item.image" />
+<!--        <a-carousel-item v-for="item in carouselItem" :key="item.title">-->
+<!--          <div :key="item.title" class="carousel-item">-->
+<!--            <div class="carousel-title">{{ item.title }}</div>-->
+<!--            <div class="carousel-sub-title">{{ item.des }}</div>-->
+<!--            <img class="carousel-image" :src="item.image"  :alt="item.title"/>-->
+<!--          </div>-->
+<!--        </a-carousel-item>-->
+        <a-carousel-item>
+          <div class="carousel-item">
+            <div class="carousel-title">京杭能源有限公司</div>
+            <div class="carousel-sub-title">京杭能源有限公司成立于2020年12月1日，重点布局新能源产业链上下游，积极发展海上风电、陆上风电、光伏发电、抽水蓄能、储能等电站的开发、投资、并购重组等业务同时，投资与新能源业务关联度高、具有优势互补和战略协同效应的相关产业，基本形成了风电、太阳能、战略投资等相互支撑、协同发展的业务布局。</div>
+            <img class="carousel-image" src="../../../assets/images/city.jpg"  alt="京杭能源有限公司"/>
+          </div>
+        </a-carousel-item>
+        <a-carousel-item>
+          <div class="carousel-item">
+            <div class="carousel-title">京杭能源有限公司</div>
+            <div class="carousel-sub-title">京杭能源牢牢把握“低碳经济，绿色发展”方向，立足新发展阶段、贯彻新发展理念、构建新发展格局，高度重视新能源的发展，不断创新新能源发电技术，充分探索新能源发电开发新模式，加强新能源发电数字化智能化建设，以新能源的快速发展引领企业高质量发展，用责任与实干践行新能源企业的使命和担当，为落实我国能源安全新战略、实现“碳达峰”“碳中和”目标做出更大贡献</div>
+            <img class="carousel-image" src="../../../assets/images/yunnan.jpg"  alt="京杭能源有限公司"/>
           </div>
         </a-carousel-item>
       </a-carousel>
